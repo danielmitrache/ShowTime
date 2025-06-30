30af0d6596d0efae7169903b5f50d3fe638a3159
Daniel Mitrache <mitracheadaniel@gmail.com>
2025-06-30 15:02:47 +0300

    can now add bands to the db

--- ShowTime/Repositories/Interfaces/IRepositoryBase.cs
+++ ShowTime/Repositories/Interfaces/IRepositoryBase.cs
@@ -4,12 +4,9 @@
     {
         Task<IEnumerable<T>> GetAllAsync();
         Task<T?> GetByIdAsync(Guid Id);
-
         Task AddAsync(T entity);
-
         void Update(T entity);
         void Delete(T entity);
-
         Task SaveChangeAsync();
     }
 }
